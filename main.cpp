5e8293ca85a09867186d36a22f00a6d682acc7a5
Zhixuan Fang <zfang18@syr.edu>
2021-04-20 10:41:30 -0400

    notify

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include<stdlib.h>
 #include<condition_variable>
 #include <time.h>
+#include<string>
 
 using namespace std;
 vector<int> load_order(5);
@@ -14,6 +15,8 @@ const vector<int> restraints{5,5,4,3,3};
 const vector<int> partW_manufacture_time{50, 50, 60, 60, 70};
 const vector<int> move_time{20, 20, 30, 30,40};
 const vector<int> prodW_assemble_time{60, 60, 70, 70, 80};
+const vector<string> status_string{"New Load Order","New Pickup Order",
+								   "Wakeup-Notified","Wakeup-Timeout"};
 mutex m1;
 condition_variable partW, productW;
 bool is_initial_state(const vector<int>&vec){//check whether if it's 0,0,0,0,0
@@ -105,12 +108,21 @@ vector<int> generate_pickup_order(vector<int>& order){
     }
     return order;
 }
-bool is_theSame(const vector<int>&vec1, const vector<int>&vec2){
-	if(vec1.size()!=vec2.size()){
+bool has_left(const vector<int>&vec){
+	for(auto const&i:vec){
+		if(i!=0){
+			return true;
+		}
+	}
+	return false;
+}
+bool is_theSame(const vector<int>& vec1, const vector<int>& vec2)
+{
+	if (vec1.size()!=vec2.size()) {
 		return false;
 	}
 	for (int i = 0; i<vec1.size(); i++) {
-		if(vec1[i]!=vec2[i]){
+		if (vec1[i]!=vec2[i]) {
 			return false;
 		}
 	}
@@ -140,15 +152,15 @@ void part_worker(int thread_id) {
 	cout << "part_worker: " << thread_id << " running" << endl;
 
 	vector<int> save_loadOrder = load_order;
-	cout << "is theSame? loadOrder: " << save_loadOrder << endl;
+	//cout << "is theSame? loadOrder: " << save_loadOrder << endl;
     vector<int> curr_loadOrder=generate_load_order(load_order);
 
-	cout << "is theSame? curr_loadOrder: " << curr_loadOrder << endl;
+	//cout << "is theSame? curr_loadOrder: " << curr_loadOrder << endl;
 	for (int i = 0; i<5;i++) {
 		//manufacture parts?
 		if(curr_loadOrder[i]!=save_loadOrder[i]){
 			int sleep_time = (curr_loadOrder[i]-save_loadOrder[i])*partW_manufacture_time[i];
-			cout << sleep_time << endl;
+			//cout << sleep_time << endl;
 			this_thread::sleep_for(chrono::microseconds(sleep_time));
 		}
 	}
@@ -158,7 +170,7 @@ void part_worker(int thread_id) {
 	cout << "Buffer State: " << Global_buffer << endl;
     if(is_overFlow(Global_buffer,curr_loadOrder)==false){//no overflow, direct merge two
 		Global_buffer = Global_buffer+curr_loadOrder;
-		for (int i = 0; i<5;i++) {//caculate move_time
+		for (int i = 0; i<5;i++) {//calculate move_time
 			if(curr_loadOrder[i]!=0){
 				int sleep_time = curr_loadOrder[i]*move_time[i];
 				this_thread::sleep_for(chrono::microseconds(sleep_time));
@@ -170,20 +182,29 @@ void part_worker(int thread_id) {
 		tmp = curr_loadOrder+Global_buffer;
 		for (int i = 0; i<5;i++) {
 			if(tmp[i]>restraints[i]){
+				int sleep_time = (restraints[i]-Global_buffer[i])*move_time[i];
+				this_thread::sleep_for(chrono::microseconds(sleep_time));
 				Global_buffer[i] = restraints[i];
 				curr_loadOrder[i] = tmp[i]-restraints[i];
 			}
 			else{
+				int sleep_time = (tmp[i]-Global_buffer[i])*move_time[i];
+				this_thread::sleep_for(chrono::microseconds(sleep_time));
 				Global_buffer[i] = tmp[i];
 				curr_loadOrder[i] = 0;
 			}
 		}
     }
+    //this place should notify another thread to
 	load_order = curr_loadOrder;
 
 	cout << "Updated Buffer State: " << Global_buffer << endl;
 	cout << "Update Load Order: " << load_order << endl;
 	cout << endl;
+	while(has_left(load_order)){
+		partW.wait(u1);
+	}
+	productW.notify_one();
 
 
 }
@@ -191,18 +212,18 @@ void part_worker(int thread_id) {
 void product_worker(int thread_id) {
 	unique_lock<mutex> u1(m1);
 	cout << "product_worker: " << thread_id << " running" << endl;
-	cout << "is theSame?: " << pickup_order << endl;
+	//cout << "is theSame?: " << pickup_order << endl;
 	vector<int> save_pickupOrder = pickup_order;
     vector<int> curr_pickupOrder=generate_pickup_order(pickup_order);
-	cout << "is theSame?: " << curr_pickupOrder << endl;
-	for (int i = 0; i<5;i++) {
-		//calculate assemble time
-		if(save_pickupOrder[i]!=curr_pickupOrder[i]){
-			int sleep_time = (curr_pickupOrder[i]-save_pickupOrder[i])*prodW_assemble_time[i];
-			cout << sleep_time << endl;
-			this_thread::sleep_for(std::chrono::microseconds (sleep_time));
-		}
-	}
+	//cout << "is theSame?: " << curr_pickupOrder << endl;
+//	for (int i = 0; i<5;i++) {
+//		//calculate assemble time
+//		if(save_pickupOrder[i]!=curr_pickupOrder[i]){
+//			int sleep_time = (curr_pickupOrder[i]-save_pickupOrder[i])*prodW_assemble_time[i];
+//			cout << sleep_time << endl;
+//			this_thread::sleep_for(std::chrono::microseconds (sleep_time));
+//		}
+//	}
     cout<<"before curr_pickupOrder is: "<<curr_pickupOrder<<endl;
 	cout << "before global_buffer is: " << Global_buffer << endl;
 	vector<int> tmp;
@@ -212,58 +233,46 @@ void product_worker(int thread_id) {
 		//...
 		Global_buffer = Global_buffer-curr_pickupOrder;
 		curr_pickupOrder={0,0,0,0,0};
+		for (int i = 0; i<5;i++) {
+			if(curr_pickupOrder[i]!=0) {
+				//move also manufacture
+				this_thread::sleep_for(chrono::microseconds(move_time[i]+prodW_assemble_time[i]));
+			}
+		}
 	}
 	else{//some pickup order cannot be fulfilled
 		tmp = Global_buffer-curr_pickupOrder;
 		//cout << "tmp is: " << tmp << endl;
 		for (int i = 0; i<5;i++) {
 			if(tmp[i]<0){
+				int sleep_time = (Global_buffer[i])*(move_time[i]+prodW_assemble_time[i]);
+				this_thread::sleep_for(chrono::microseconds(sleep_time));
 				Global_buffer[i] = 0;
 				curr_pickupOrder[i] = (-tmp[i]);
 			}
 			else{
+				int sleep_time = curr_pickupOrder[i]*(move_time[i]+prodW_assemble_time[i]);
+				this_thread::sleep_for(chrono::microseconds(sleep_time));
 				Global_buffer[i] = tmp[i];
 				curr_pickupOrder[i] = 0;
 			}
 		}
 	}
 	pickup_order = curr_pickupOrder;
-	cout<<"after curr_pickupOrder is: "<<pickup_order<<endl;
-	cout << "after global_buffer is: " << Global_buffer << endl<<endl;
+
+	cout<<"Updated pickup order: "<<pickup_order<<endl;
+	cout << "Updated Buffer State: " << Global_buffer << endl<<endl;
+	while(has_left(pickup_order)){
+		productW.wait(u1);
+	}
+	partW.notify_one();
 
 }
 
 
 
 int main(){
-//	cout << "curr_loadorder: " << load_order << endl;
-//	cout <<"Global: " <<Global_buffer << endl;
-//	cout << "curr_pickup: " << pickup_order << endl<<endl;
-//    part_worker(1);
-//	cout << "curr_loadorder: " << load_order << endl;
-//	cout <<"Global: " <<Global_buffer << endl;
-//	cout << "curr_pickup: " << pickup_order << endl<<endl;
-//    product_worker(2);
-//	cout << "curr_loadorder: " << load_order << endl;
-//	cout <<"Global: " <<Global_buffer << endl;
-//	cout << "curr_pickup: " << pickup_order << endl<<endl;
-//    vector<int> try_generate_load_order=generate_order(load_order);
-//    cout<<"try generate load order: " <<try_generate_load_order<<endl;
-//    cout<< is_initial_state(Global_buffer)<<endl;
-//    srand((unsigned)time(NULL));
-//    int someRandnumber=rand()%5;
-    //int someR=rand()%5;
 
-//    cout << someRandnumber<<endl;
-//    cout << "curr_Global_buffer is: "<<Global_buffer<<endl;
-//    vector<int> a1{0,1,1};
-//    vector<int> a2{2,2,2};
-//    vector<int> adding_res=a1+a2;
-//    cout << adding_res<<endl;
-//    vector<int> minus_res=a1-a2;
-//    cout<<minus_res<<endl;
-   // vector<int> try_gen_pickup_order=generate_pickup_order(vector<int> pickup_order);
-    //cout<<"adding is: "<<res<<endl;
     const int m = 20, n = 16; //m: number of Part Workers
 //n: number of Product Workers
 //m>n
